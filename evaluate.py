6c6b16ed4ae3281517d31eff314d1d442f49ca97
Lopara03 <lopara@terpmail.umd.edu>
2025-08-09 10:53:00 -0400

    Add files via upload
    uploading my code manually to the dev branch

---
+++ evaluate.py
@@ -0,0 +1,18 @@
+
+"""
+this module evaluates the performance of the model using various metrics.
+"""
+
+def evaluate_model(model, X_test, y_test):
+    """
+    evaluates the trained model on test data.
+
+    args:
+        model: trained model.
+        X_test: test features.
+        y_test: true labels.
+
+    returns:
+        dict: evaluation metrics (accuracy, precision, recall, etc.)
+    """
+    pass
---
+++ load.py
@@ -0,0 +1,14 @@
+
+"""
+this module saves the cleaned data to the processed data folder.
+"""
+
+def save_data(df, filepath):
+    """
+    saves the cleaned DataFrame to a specified location.
+
+    args:
+        df (DataFrame): cleaned data.
+        filepath (str): destination file path.
+    """
+    pass  # replace with df.to_csv(filepath) later
