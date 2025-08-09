f5e4360c946efd728ce0d7dff4cb8ff2d6cd6ce7
Lopara03 <lopara@terpmail.umd.edu>
2025-08-09 11:06:28 -0400

    visualization.py cide

---
+++ vis/visualization.py
@@ -0,0 +1,13 @@
+"""
+this module creates visualizations for data analysis and model results.
+"""
+
+def plot_distribution(df, column):
+    """
+    plots a histogram for a given column.
+
+    args:
+        df (DataFrame): dataset.
+        column (str): column to plot.
+    """
+    pass  # later: use matplotlib/seaborn
